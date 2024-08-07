d66fe3bc7632c4f6bac0fcb1e8a92a5c8613a899
Isaac Ayomide <okunlolaa3@outlook.com>
2024-08-07 21:51:44 +0100

    project completed

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -6,33 +6,8 @@ const diceIcon = document.querySelector(".dice");
 const adviceContainer = document.querySelector("main");
 
 const renderQuotes = function (data) {
-  const quoteNum = data.slip.id;
-  const advice = data.slip.advice;
-
-  const html = `
-  <main>
-  <div class="advice__heading">
-    <h1>ADVICE #<span class="advice__no">${quoteNum}</span></h1>
-  </div>
-  <div class="advice__text">
-    <p class="quote">
-      "${advice}"
-    </p>
-  </div>
-  <div class="pattern__divider">
-    <img
-      src="./images/pattern-divider-mobile.svg"
-      alt="pattern-divider"
-      class="divider"
-    />
-  </div>
-
-  <div class="dice__icon">
-    <img src="./images/icon-dice.svg" alt="dice-icon" class="dice" />
-  </div>
-</main>
-    `;
-  adviceContainer.insertAdjacentHTML("beforeend", html);
+  quoteNum.textContent = data.slip.id;
+  quote.textContent = data.slip.advice;
 };
 
 const getQuote = function () {
